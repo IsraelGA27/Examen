7d855d1e156718d041651b44f74a639e1233b0fd
Israel García Agüero <ig239503@gmail.com>
2022-03-15 19:09:35 -0600

    Fundamental para el inicio Ruleta para apostar con cualquier número

--- Examen/Program.cs
+++ Examen/Program.cs
@@ -19,31 +19,63 @@ namespace Examen
             // Saldo con el que empieza el jugador
             int saldo = 300;  // Si para solo 300, es pobre el pana
 
-            Console.WriteLine("Tu saldo es de: " + saldo + " Pesos ");
-
-            if (saldo <= 0)
+            while (true) // Para poder usar: break y continue
             {
-                Console.WriteLine("Carnal no tienes dinero, largate");
-                //break;
+                Console.WriteLine();
+                Console.WriteLine("Tu saldo es de: " + saldo + " Pesos ");
+
+                if (saldo <= 0)
+                {
+                    Console.WriteLine("Carnal no tienes dinero, largate");
+                    break;
+                }
+
+                // Lo que vamos a apostar - empezaremos por cualquier número.
+                Console.WriteLine();
+                Console.Write("¿Que cantidad vas a apostar?    /////     -1(SALIR)");
+                Console.WriteLine();
+
+                //int apuesta; 
+                int apuesta = int.Parse(Console.ReadLine());
+
+                if (apuesta == -1)
+                {
+                    break;
+                }
+                if (apuesta > saldo)
+                {
+                    Console.WriteLine("No tienes dinero");
+                    continue;
+                }
+
+
+
+                Console.WriteLine();
+                Console.WriteLine("Ingresa el numero al que deseas apostar; ");
+                int numeroapostado = int.Parse(Console.ReadLine());
+
+
+                // Generamos numeros aleatorios
+                Random Generar = new Random();
+                int aleatorio = Generar.Next(1, 2);
+
+
+                if (numeroapostado == aleatorio)
+                {
+                    Console.WriteLine("!Felicidades¡ Ha caido tu numero ");
+                    saldo += apuesta * 10;
+                    Console.WriteLine("Tu nuevo saldo es de: " + saldo);
+                    Console.WriteLine("Presiona cualquier tecla para continuar :D  ");
+                }
+                else
+                {
+                    Console.WriteLine("Has pedido, ah caido el numero: " + aleatorio);
+                    saldo -= apuesta;
+                    Console.WriteLine("Tu nuevo saldo es de: " + saldo);
+                    Console.WriteLine("Presiona cualquier tecla para continuar :D  ");
+                }
+                Console.ReadKey();
             }
-
-            // Lo que vamos a apostar - empezaremos por cualquier número.
-            Console.WriteLine();
-            Console.Write("¿Que cantidad vas a apostar?    /////   Usa -1(SALIR)");
-            Console.WriteLine();
-
-            //int apuesta; 
-            int apuesta = int.Parse(Console.ReadLine());
-
-            if (apuesta == -1)
-            {
-                break;
-            }
-            else
-            {
-
-            }
-
         }
     }
 }
