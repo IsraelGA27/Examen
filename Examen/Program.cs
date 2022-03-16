6f7f227058b60adb7298caa5d07208d064cc64e8
Israel García Agüero <ig239503@gmail.com>
2022-03-15 23:21:32 -0600

    Ultimo (Realmente no tiene nada pero por las dudas)

--- Examen/Program.cs
+++ Examen/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// Lo maximo que se pudo 
+// Lo maximo que se pudo :D
 namespace Examen
 {
     class Program
